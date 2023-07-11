4b2d7fce4b0f32cd8263fe2e6b067b6fa47bfc41
Kirti Singh <kirtisingh1497@gmail.com>
2023-07-11 17:24:32 +0530

    added responsiveness

--- src/Component/PlaceToVisit.js
+++ src/Component/PlaceToVisit.js
@@ -17,13 +17,13 @@ export default function PlaceToVisit() {
         
         <div className='place-img'><img src={image} alt={name}/></div>
         <div>
-            <p className='place'><span className='name'>Description:</span><span>{description}</span></p>
-            <p className='place'><span className='name'>Rating:</span><span>{ratings}</span></p>
-            <p className='place'><span className='name'>Location:</span><span>{location}</span></p>
-            <p className='place'><span className='name'>Opening Hours:</span>{openingHours}<span></span></p>
-            <p className='place'><span className='name'>Reviews:</span><span>{reviews}</span></p>
-            <p className='place'><span className='name'>ticket Price:</span><span>{ticketPrice}</span></p>
-            <p className='place'><span className='name'>Website:</span><span><a href={website} />{website}</span></p>
+            <p className='place'><span className='name'>Description:</span><span className='detail'>{description}</span></p>
+            <p className='place'><span className='name'>Rating:</span><span className='detail'>{ratings}</span></p>
+            <p className='place'><span className='name'>Location:</span><span className='detail'>{location}</span></p>
+            <p className='place'><span className='name'>Opening Hours:</span ><span className='detail'>{openingHours}</span></p>
+            <p className='place'><span className='name'>Reviews:</span><span className='detail'>{reviews}</span></p>
+            <p className='place'><span className='name'>ticket Price:</span><span className='detail'>{ticketPrice}</span></p>
+            <p className='place'><span className='name'>Website:</span><span className='detail'><a href={website} />{website}</span></p>
         </div>
         </div>
         
